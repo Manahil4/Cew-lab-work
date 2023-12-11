57baa66c0c74f1831a36a2e9265e1fe2e728e1b5
Manahil Ejaz <142149713+Manahil4@users.noreply.github.com>
2023-12-11 11:55:42 +0500

    Update Làb6.c

--- Làb6.c
+++ Làb6.c
@@ -16,11 +16,11 @@ readFromFile(fname);
 #include <stdio.h>
 #include "lab6.h"
 // Function to write a string 'th' to the file 'fname'
-void writeToFile(char *th,const char *fname ) {
+void writeToFile(char *th ) {
     FILE *fp;
     
     // Open the file in append mode
-    fp = fopen(fname, "a");
+    fp = fopen("lab6.txt", "a");
     
     // Check if the file is successfully opened
     if (fp != NULL) {
@@ -36,29 +36,49 @@ void writeToFile(char *th,const char *fname ) {
 }
 
 // Function to read a string from the specified file 'fname'
+
+
 char* readFromFile(const char *fname) {
-    char Mano[300];
     FILE *f;
-    
-    // Open the file in read mode
+    char *Mano = NULL;  // Initialize to NULL to handle allocation failure
+    long file_size;
+
     f = fopen(fname, "r");
     
-    // Check if the file is successfully opened
     if (f != NULL) {
-        // Read a string from the file
-        fscanf(f, "%s", Mano);
-        
-        // Print the string read from the file
-        printf("%s\n", Mano);
-        
-        // Close the file after reading
+        // Get the file size
+        fseek(f, 0, SEEK_END);
+        file_size = ftell(f);
+        rewind(f);
+
+        // Allocate memory for the content
+        Mano = (char*)malloc((file_size + 1) * sizeof(char));
+
+        if (Mano != NULL) {
+            // Read the entire file into the Mano buffer using fgetc
+            int c;
+            size_t i = 0;
+            while ((c = fgetc(f)) != EOF && i < file_size) {
+                Mano[i++] = (char)c;
+            }
+            
+            // Null-terminate the content
+            Mano[i] = '\0';
+
+            // Print or process the content
+            printf("%s", Mano);
+        } else {
+            // Memory allocation error
+            printf("Memory allocation error.\n");
+        }
+
+        // Close the file
         fclose(f);
     } else {
-        // Handle the case where the file couldn't be opened
+        // Error opening the file
         printf("Error opening the file for reading.\n");
     }
-    
-    // Return the read string
+
     return Mano;
 }
 
